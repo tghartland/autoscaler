36417bb96bedda49f3bdea075d680fe2256a5f89
Thomas Hartland <thomas.george.hartland@cern.ch>
2020-01-31 11:56:45 +0100

    [cern] Update gophercloud for magnum cluster resize API

--- cluster-autoscaler/cloudprovider/magnum/gophercloud/openstack/identity/v3/extensions/trusts/urls.go
+++ cluster-autoscaler/cloudprovider/magnum/gophercloud/openstack/identity/v3/extensions/trusts/urls.go
@@ -1,8 +1,8 @@
-package floatingips
+package trusts
 
 import "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/magnum/gophercloud"
 
-const resourcePath = "floatingips"
+const resourcePath = "OS-TRUST/trusts"
 
 func rootURL(c *gophercloud.ServiceClient) string {
 	return c.ServiceURL(resourcePath)
@@ -10,4 +10,12 @@ func rootURL(c *gophercloud.ServiceClient) string {
 
 func resourceURL(c *gophercloud.ServiceClient, id string) string {
 	return c.ServiceURL(resourcePath, id)
+}
+
+func createURL(c *gophercloud.ServiceClient) string {
+	return rootURL(c)
+}
+
+func deleteURL(c *gophercloud.ServiceClient, id string) string {
+	return resourceURL(c, id)
 }
